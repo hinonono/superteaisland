acc7a05f1cae2b9a8190c7652c7297cb58f8b299
Hsieh <johnny.hsieh.12@gmail.com>
2022-04-23 13:05:41 +0800

    change popup image to final art and add go to scene script

--- teaisland/Assets/Scripts/GoToScene.cs
+++ teaisland/Assets/Scripts/GoToScene.cs
@@ -5,8 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class GoToScene : MonoBehaviour
 {
-    public void ButtonMoveScene(string destination)
+    public GameObject popup;
+    private string sceneDestination;
+
+    public void SetSceneDestination(string destination)
     {
-        SceneManager.LoadScene(destination);
+        popup.SetActive(true);
+        sceneDestination = destination;
+    }
+
+    public void ButtonMoveScene()
+    {
+        if (sceneDestination != null)
+        {
+            SceneManager.LoadScene(sceneDestination);
+        }
+        else
+        {
+            Debug.Log("Scene Destination is not set yet. Try run SetSceneDestination() first.");
+        }
+        
+    }
+
+    public void OpenPopup()
+    {
+        popup.SetActive(true);
+    }
+
+    public void ClosePopup()
+    {
+        popup.SetActive(false);
     }
 }
