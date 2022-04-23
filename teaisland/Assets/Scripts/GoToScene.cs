8f6a3124c8b7ef7087ac8239291fffdcf4f67142
Hsieh <johnny.hsieh.12@gmail.com>
2022-04-23 15:32:16 +0800

    add ui animation using asset leantween

--- teaisland/Assets/Scripts/GoToScene.cs
+++ teaisland/Assets/Scripts/GoToScene.cs
@@ -1,17 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GoToScene : MonoBehaviour
 {
+    public float animateSpeed = 0.5f;
     public GameObject popup;
     private string sceneDestination;
+
+    public Transform box;
+    public CanvasGroup background;
+    public Text areaName;
 
     public void SetSceneDestination(string destination)
     {
         popup.SetActive(true);
         sceneDestination = destination;
+
+        switch (destination)
+        {
+            case "Map_red":
+                areaName.text = "紅茶之境";
+                break;
+
+            default:
+                areaName.text = "Unknown";
+                break;
+        }
+
+        background.alpha = 0;
+        background.LeanAlpha(1, animateSpeed);
+
+        box.localPosition = new Vector2(0, -Screen.height);
+        box.LeanMoveLocalY(0, animateSpeed).setEaseOutExpo().delay = 0.1f;
     }
 
     public void ButtonMoveScene()
@@ -27,12 +50,13 @@ public class GoToScene : MonoBehaviour
         
     }
 
-    public void OpenPopup()
+    public void ClosePopup()
     {
-        popup.SetActive(true);
+        background.LeanAlpha(0, animateSpeed);
+        box.LeanMoveLocalY(-Screen.height, animateSpeed).setEaseInExpo().setOnComplete(onComplete);
     }
 
-    public void ClosePopup()
+    void onComplete()
     {
         popup.SetActive(false);
     }
